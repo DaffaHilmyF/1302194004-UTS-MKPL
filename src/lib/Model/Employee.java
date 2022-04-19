170be97e08554f78e594598d53c921ff06232834
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2022-04-20 01:51:45 +0700

    Menambahkan dan menghapus beberapa attribute pada class employee
    1. Menambahkan getter untuk setiap attribur
    
    2. Memindahkan attribute gender kedalam class user
    
    3. Menghapus parameter pada contructor

--- src/lib/Model/Employee.java
+++ src/lib/Model/Employee.java
@@ -1,21 +1,17 @@
 package lib.Model;
 
 import java.time.LocalDate;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.UUID;
 
 import lib.TaxFunction;
 
 public class Employee {
 
+	private String employeeId;
 	private User employeeDetail;
 	private Spouse employeeSpouse;
 	private List<Child> employeeChild;
-
-	private String employeeId;
-	private boolean isForeigner;
-	private LocalDate dateJoined;
+	private LocalDate employeeDateJoined;
 	private int monthWorkingInYear;
 
 	public Employee(User employeeDetail, Spouse employeeSouse, List<Child> employeeChild) {
@@ -25,19 +21,40 @@ public class Employee {
 		this.employeeChild = employeeChild;
 	}
 
+	public String getEmployeeId() {
+		return this.employeeId;
+	}
+
+	public User getEmployeeDetail() {
+		return this.employeeDetail;
+	}
+
+	public Spouse getEmployeeSpouse() {
+		return this.employeeSpouse;
+	}
+
+	public List<Child> getEmployeeChild() {
+		return this.employeeChild;
+	}
+
+	public LocalDate getEmployeDateJoined() {
+		return this.employeeDateJoined;
+	}
+
 	public int getAnnualIncomeTax() {
 
 		// Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah
 		// bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 
-		if (date.getYear() == dateJoined.getYear()) {
+		if (date.getYear() == employeeDateJoined.getYear()) {
 			monthWorkingInYear = date.getMonthValue() - date.getMonthValue();
 		} else {
 			monthWorkingInYear = 12;
 		}
-
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible,
-				employeeSpouse.getId().equals(""), childList.size());
+		return 0;
+		// return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome,
+		// monthWorkingInYear, annualDeductible,
+		// employeeSpouse.getId().equals(""), childList.size());
 	}
 }
