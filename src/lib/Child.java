2781e1932ceb2faa795541532fb41729b52736a4
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2022-04-19 23:44:51 +0700

    Menambahkan constructor dan getter setter
    Menambahkan constructor dan getter setter pada dua attribute name dan id

--- src/lib/Child.java
+++ src/lib/Child.java
@@ -3,4 +3,25 @@ package lib;
 public class Child {
     private String name;
     private String id;
+
+    public Child(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
