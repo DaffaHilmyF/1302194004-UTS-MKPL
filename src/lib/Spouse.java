9f146bc8535cbe09ee2f35bc0984c03b37f2589f
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2022-04-19 23:36:52 +0700

    Mengubah penamaan variable pada kelas Spouse
    1. Menggubah penamaan variable dari spouseName ke variable name
    
    2. Menggubah penamaan variable dari spouseId ke variable id
    
    3. Menambahkan constructor dan getter setter

--- src/lib/Spouse.java
+++ src/lib/Spouse.java
@@ -1,6 +1,27 @@
 package lib;
 
 public class Spouse {
-    private String spouseName;
-    private String spouseIdNumber;
+    private String name;
+    private String id;
+
+    public Spouse(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
